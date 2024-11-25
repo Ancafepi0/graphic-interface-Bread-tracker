bb90454ccd34fffb80a75f213ade025ba6272834
camilofernandezuno@gmail.com <camilofernandezuno@gmail.com>
2024-11-25 11:08:22 -0500

    Inventario casi terminado

--- script/main.js
+++ script/main.js
@@ -83,3 +83,20 @@ $(document).ready(function(){
         });
     });
 })(jQuery);
+$(document).ready(function() {
+    const $filtro = $('#filtro');
+    const $menuFiltro = $('#menu-filtro');
+
+    // Mostrar el filtro cuando se haga clic en el icono
+    $menuFiltro.on('click', function(e) {
+        e.stopPropagation(); // Evita que se propague el evento y cierre el filtro inmediatamente
+        $filtro.toggle(); // Alternar entre mostrar y ocultar el filtro
+    });
+
+    // Cerrar el filtro si se hace clic fuera de él
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('#filtro, #menu-filtro').length) {
+            $filtro.hide(); // Ocultar el filtro si el clic no está dentro del filtro ni en el icono
+        }
+    });
+});
